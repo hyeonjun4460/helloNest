59dbe7717f407b4beb86d8da0ef6b3479131413e
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-08-07 16:46:32 +0900

    [feat] 고양이 블로그 C,R 구현

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,60 @@
 import * as express from "express";
+import { nextTick } from "process";
 import { Cat } from "./app.model";
 const app: express.Express = express();
 
 const port: number = 8000;
 
+// 로깅
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   next();
 });
 
-app.get("/", (req, res) => {
-  throw new Error("에러났음");
-  res.status(200).send({
-    Cat
-  });
+// json
+app.use(express.json());
+
+// 데이터 전체 조회
+app.get("/cats", (req, res, next) => {
+  try {
+    const cats = Cat;
+    res.status(200).send({
+      success: true,
+      cats
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
+// 상세 조회
+
+app.get("/cats/:id", (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const cat = Cat.find((value) => {
+      return value.id === id;
+    });
+    res.send({
+      success: true,
+      cat
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/cats", (req, res, next) => {
+  try {
+    const data = req.body;
+    Cat.push(data);
+    res.status(200).send({ success: true });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 에러 헨들러
 app.use(
   (
     error: Error,
