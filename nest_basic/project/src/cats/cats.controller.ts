724cf4e30b2afa14d3c19232d37d2bdbe1f68c68
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-08-15 16:25:17 +0900

    [feat] 404에러 포함한 에러 미들웨어 생성

--- nest_basic/project/src/cats/cats.controller.ts
+++ nest_basic/project/src/cats/cats.controller.ts
@@ -1,12 +1,18 @@
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Controller, Delete, Get, HttpException, Patch, Post, UseFilters } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getCats(): string {
+    // nest에서 예외 에러 발생에 활용되는 인터페아스: HttpException = new Error()
+    // 에러는 오버라이딩 해서 커스텀할 수 있음.
+    // 예외처리는 모든 컨트롤러에 적는 것이 아니라, 예외처리 미들웨어를 생성하여 일괄적으로 처리하게 함. => 코드 재사용성 up
+    throw new HttpException('api is broken', 401);
     return this.catsService.getCats();
   }
 
