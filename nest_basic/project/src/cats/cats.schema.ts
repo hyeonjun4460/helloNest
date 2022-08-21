6dad7696fb7939450a47743fa9bb2ae4c3e69687
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-08-21 22:23:11 +0900

    [feat] schema에 class-validator 적용.

---
+++ nest_basic/project/src/cats/cats.schema.ts
@@ -0,0 +1,39 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaOptions } from 'mongoose';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'; // 클래스 벨리데이터, 클래스에 삽입되는 멤버변수들의 validation.
+// 스키마와 관련된 옵션
+const options: SchemaOptions = {
+  timestamps: true, // DB에서 스키마 생성 시 일자를 출력
+};
+
+@Schema(options)
+export class Cat extends Document {
+  @Prop({
+    //스키마에 들어가는 각 데이터들의 옵션 설정
+    required: true,
+    unique: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop()
+  @IsString()
+  imgUrl: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
